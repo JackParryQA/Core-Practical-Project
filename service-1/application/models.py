b6e55a225ffdc9642d42722ee667f66a21577b38
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-06-07 17:31:41 +0100

    Update models.py

--- service-1/application/models.py
+++ service-1/application/models.py
@@ -2,5 +2,6 @@ from application import db
 
 class MLBDraft(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    team = db.Column(db.String(50), nullable=False)
     position = db.Column(db.String(2), nullable=False)
     pick = db.Column(db.Integer, nullable=False)
