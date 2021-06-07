dbaaa1125f446a7085e1ee1c5765ac6704498d6d
Jack Parry <jparry@qa.com>
2021-06-07 16:06:23 +0000

    changes to service-1

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -7,5 +7,9 @@ import requests
 def index():
     picks = MLBDraft()
     all_picks = picks.query.all()
-    response = requests.get('http://service_4:5003').text
+    position = requests.get('http://service-2:5001').text
+    pick = requests.get('http://service-3:5002').text
+
+    response = requests.post('http://service-4:5003',json={position,pick}).json
+
     return render_template('index.html', response=response, picks=all_picks)
