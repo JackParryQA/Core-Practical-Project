6c40ce395dba36b10fa2e6fc446dd970a2e6bbbe
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-06-07 17:13:30 +0100

    Update routes.py

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -1,15 +1,15 @@
 from application import app, db
 from flask import render_template
 from application.models import MLBDraft
-import request
+import requests
 
 @app.route('/', methods=['GET','POST'])
 def index():
     picks = MLBDraft()
     all_picks = picks.query.all()
-    position = request.get('http://service-2:5001').text
-    pick = request.get('http://service-3:5002').text
+    position = requests.get('http://service-2:5001').text
+    pick = requests.get('http://service-3:5002').text
 
-    response = request.post('http://service-4:5003',json={position,pick}).json
+    response = requests.post('http://service-4:5003',json={'position': position, 'pick': pick}).json
 
     return render_template('index.html', response=response, picks=all_picks)
