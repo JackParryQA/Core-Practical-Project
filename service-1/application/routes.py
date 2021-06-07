2b95b96bdff6cd6ef8e88c93a3cdf1dd1e45b26c
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-06-07 17:30:38 +0100

    Update routes.py

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -7,9 +7,10 @@ import requests
 def index():
     picks = MLBDraft()
     all_picks = picks.query.all()
-    position = requests.get('http://service-2:5001').text
-    pick = requests.get('http://service-3:5002').text
+    position = requests.get('http://service-2:5001/position').text
+    pick = requests.get('http://service-3:5002/pick').text
+    team = requests.get('http://service-4:5003/Get Team')
 
-    response = requests.post('http://service-4:5003',json={'position': position, 'pick': pick}).json
+    response = requests.post('http://service-4:5003/Draft Pick',json={'position': position, 'pick': pick}).json
 
     return render_template('index.html', response=response, picks=all_picks)
