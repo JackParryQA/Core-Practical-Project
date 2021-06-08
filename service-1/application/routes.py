91724e439b79f5f3c90ba35711767ccd321efca6
Jack Parry <jparry@qa.com>
2021-06-08 14:50:30 +0000

    Database implemented

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -2,19 +2,21 @@ from application import app, db
 from flask import render_template
 from application.models import MLBDraft
 import requests
+from sqlalchemy import desc
 
 @app.route('/', methods=['GET','POST'])
 def index():
     picks = MLBDraft()
-    # all_picks = picks.query.all()
+    
     position = requests.get('http://service-2:5000/position').text
     pick = requests.get('http://service-3:5000/pick').text
     # team = requests.get('http://service-4:5003/Get Team').text
 
     team = requests.post('http://service-4:5000/Draft Pick',json={'position': position, 'pick': int(pick)}).text
-    print(team)
-    # new_pick = MLBDraft(team=team,position=position,pick=pick)
-    # db.session.add(new_pick)
-    # db.session.commit()
 
-    return render_template('index.html',  team=team, pick=pick, position=position)
+    all_picks = MLBDraft.query.order_by(desc(MLBDraft.id)).limit(5).all()
+    new_pick = MLBDraft(team=team,position=position,pick=pick)
+    db.session.add(new_pick)
+    db.session.commit()
+
+    return render_template('index.html',  team=team, pick=pick, position=position, picks=all_picks)
