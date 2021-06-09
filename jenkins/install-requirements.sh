45931975b8dd4ccb3cfadffebda4332bf4e8b223
Jack Parry <jparry@qa.com>
2021-06-09 16:40:04 +0000

    edit instal-req

--- jenkins/install-requirements.sh
+++ jenkins/install-requirements.sh
@@ -22,7 +22,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo apt update 
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
 
 # Docker login
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
