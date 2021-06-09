a558f410a3f05e0f32d9b90f4cdf9f94bceeab22
Jack Parry <jparry@qa.com>
2021-06-09 15:30:28 +0000

    created ansible playbook, inventory & roles

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -8,10 +8,10 @@ from sqlalchemy import desc
 def index():
     picks = MLBDraft()
     
-    position = requests.get('http://service-2:5000/position').text
-    pick = requests.get('http://service-3:5000/pick').text
+    position = requests.get('http://service-2:5001/position').text
+    pick = requests.get('http://service-3:5002/pick').text
 
-    response = requests.post('http://service-4:5000/draft pick',json={'position': position, 'pick': int(pick)})
+    response = requests.post('http://service-4:5003/draft pick',json={'position': position, 'pick': int(pick)})
     response_json=response.json()
     all_picks = MLBDraft.query.order_by(desc(MLBDraft.id)).limit(5).all()
     new_pick = MLBDraft(team=response_json['team'],position=position,pick=pick)
