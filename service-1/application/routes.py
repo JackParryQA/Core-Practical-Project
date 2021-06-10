0fbad9f858863b29515e33ce106faf9577624b45
Jack Parry <jparry@qa.com>
2021-06-10 12:17:54 +0000

    sorted dev branch

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -9,9 +9,9 @@ def index():
     picks = MLBDraft()
     
     position = requests.get('http://service-2:5001/position').text
-    pick = requests.get('http://service-3:5002/pick').json()
+    pick = requests.get('http://service-3:5002/pick').text
 
-    response = requests.post('http://service-4:5003/draft pick',json={'position': position, 'pick': pick['pick']})
+    response = requests.post('http://service-4:5003/draft pick',json={'position': position, 'pick': int(pick)})
     response_json=response.json()
     all_picks = MLBDraft.query.order_by(desc(MLBDraft.id)).limit(5).all()
     new_pick = MLBDraft(team=response_json['team'],position=position,pick=pick)
