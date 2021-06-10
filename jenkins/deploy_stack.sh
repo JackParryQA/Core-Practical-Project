708d43fe801d6ef813336c14b7e148875ac14479
Jack Parry <jparry@qa.com>
2021-06-10 09:09:54 +0000

    changed deploy_stack.sh

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -2,5 +2,5 @@
 scp -i /home/jenkins/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager-1:~/docker-compose.yaml
 ssh -i /home/jenkins/.ssh/ansible_id_rsa jenkins@manager-1 << EOF
     export DATABASE_URI=${DATABASE_URI}
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml service-1 service-2 service-3 service-4
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml MLBDraft
 EOF
