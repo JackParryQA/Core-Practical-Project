39c8b5f5384a5dd71baff7de1200db3acb449cd1
Jack Parry <jparry@qa.com>
2021-06-10 08:45:17 +0000

    changed file path for private key in deploy_stack.sh

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml manager-1@manager-1:~/docker-compose.yaml
-ssh -i ~/.ssh/ansible_id_rsa manager-1@manager-1 << EOF
+scp -i /home/jenkins/.ssh/ansible_id_rsa docker-compose.yaml manager-1@manager-1:~/docker-compose.yaml
+ssh -i /home/jenkins/.ssh/ansible_id_rsa manager-1@manager-1 << EOF
     export DATABASE_URI=${DATABASE_URI}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml
 EOF
