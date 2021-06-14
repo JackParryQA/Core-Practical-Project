aff6a0020fadc0852a20ae4a7b0b71abd8040e79
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-06-14 14:41:51 +0100

    Implementation 2 (#28)
    * Made changes to code for implementation-2
    
    * Fixed spelling mistake in service-4
    
    * edit Jenkinsfile
    
    * Edit Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update docker-compose.yaml
    
    * Update Jenkinsfile

--- service-1/application/models.py
+++ service-1/application/models.py
@@ -3,5 +3,5 @@ from application import db
 class MLBDraft(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     team = db.Column(db.String(50), nullable=False)
-    position = db.Column(db.String(2), nullable=False)
+    position = db.Column(db.String(50), nullable=False)
     pick = db.Column(db.Integer, nullable=False)
